027003f2e87465f78ce011dac49337fec124149a
linerocks <tirtadwipa.manunggal@gmail.com>
2023-01-20 10:31:25 +0700

    add uint8array to hex for keypair

--- encryption-asymmetric.js
+++ encryption-asymmetric.js
@@ -16,8 +16,16 @@ const sharedB = box.before(pairA.publicKey, pairB.secretKey);
 const encrypted = encrypt(sharedA, obj);
 const decrypted = decrypt(sharedB, encrypted);
 
+const toHexString = (bytes) =>
+  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+const fromHexString = (hexString) =>
+  Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+
+
 console.log("Object", obj);
 console.log("Pair A", pairA);
+console.log("Pair A public key hex", toHexString(pairA.publicKey));
+console.log("Pair A public key", fromHexString(toHexString(pairA.publicKey)));
 console.log("Pair B", pairB);
 console.log("Shared A", sharedA);
 console.log("Shared B", sharedB);
